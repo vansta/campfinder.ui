dad3f43b9e56cb772400c3c8f9c583f53fd427fb
Stan <stan.vancraeynest@gmail.com>
2020-01-16 21:12:31 +0100

    first commit

---
+++ src/components/terrains-overview.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="terrains-overview">
+    <h1>terrains-overview Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'terrains-overview',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .terrains-overview {
+
+  }
+</style>

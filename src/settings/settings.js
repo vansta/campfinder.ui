7d57c5edb0cc2467ed41185a9a8ffd96cecfa5d1
Stan <stan.vancraeynest@gmail.com>
2020-03-18 18:58:47 +0100

    stan: added settings for build

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,5 +1,6 @@
 const settings = {
-    baseUrlApi: 'https://localhost:44395/api'
+    baseUrlApi: 'https://localhost:44395/api',
+    //baseUrlApi: 'http://192.168.1.12:8080/api'
 }
 
 export default settings

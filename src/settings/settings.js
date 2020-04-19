f9f6c6149913bd03d3f0e3152d8a480182ef1de5
Stan <stan.vancraeynest@gmail.com>
2020-04-19 11:30:58 +0200

    correct settings syntax

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,7 +1,7 @@
 const settings = {
     // baseUrlApi: 'https://localhost:44395/api'
     // baseUrlApi: 'http://192.168.0.140:5000/api'
-    baseuUrlApi: 'http://81.165.156.53:5000/api'
+    baseUrlApi: 'http://81.165.156.53:5000/api'
 }
 
 export default settings

9351eb4eb406c6875a13d09fb7e7e12ce442bcb3
Stan <stan.vancraeynest@gmail.com>
2021-01-21 18:02:20 +0100

    settings

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,7 +1,9 @@
 const settings = {
     // baseUrlApi: 'https://localhost:44395/api'
     // baseUrlApi: 'http://192.168.0.140:5000/api'
-    baseUrlApi: 'http://81.165.156.53:5000/api'
+    // baseUrlApi: 'http://81.165.156.53:5000/api'
+    baseUrlApi: 'http://161.97.179.116:5001/api'
+
 }
 
 export default settings

40dce2802a1fe4fb3957eaee3147d51a4ccd66b9
Stan <stan.vancraeynest@gmail.com>
2021-01-22 18:58:00 +0100

    settings from env

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -2,8 +2,8 @@ const settings = {
     // baseUrlApi: 'https://localhost:44395/api'
     // baseUrlApi: 'http://192.168.0.140:5000/api'
     // baseUrlApi: 'http://81.165.156.53:5000/api'
-    baseUrlApi: 'http://161.97.179.116:5001/api'
-
+    // baseUrlApi: 'http://161.97.179.116:5001/api'
+    baseUrlApi: process.env.VUE_APP_BASEURL
 }
 
 export default settings
